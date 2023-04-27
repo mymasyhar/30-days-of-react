e15d3c8fef4862c7fbc429998f85b8bfd1932e7e
mymasyhar <mymasyhar@gmail.com>
2023-04-27 14:10:13 +0700

    day-1-javascript-refresher-[initializing repo to github]

---
+++ day-1-javascript-refresher/exercise-1/script.js
@@ -0,0 +1,29 @@
+// ------------------------------ level 1 -------------------------
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+];
+
+// 1. Declare an empty array;
+    console.log(`Declare an empty array;`);
+    const newArray = [];
+    console.log(newArray);
