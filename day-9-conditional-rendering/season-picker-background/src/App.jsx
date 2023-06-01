348ed33495c6bacf8be9bb9a5341fc4381c57898
mymasyhar <mymasyhar@gmail.com>
2023-06-01 07:22:12 +0700

    day-9-conditional-rendering-[rebuild the project]

--- day-9-conditional-rendering/season-picker-background/src/App.jsx
+++ day-9-conditional-rendering/season-picker-background/src/App.jsx
@@ -1,77 +1,29 @@
-import { Component } from "react";
+import React, { Component } from 'react'
+import { useState } from 'react'
 
-const buttonStyles = {
-  padding: 10,
-  border: 'none',
-}
-const articleStyles = {
-  padding: 10,
-  border: "none",
-  fontFamily: "Montserrat",
-  background: "#EFB503",
-};
-const buttonThemes = {
-  autumn: '#C07F00',
-  winter: '#D4FAFC',
-  spring: '#A4D0A4',
-  summer: '#FFD95A',
-}
-class App extends Component {
-  state = {
-    ...articleStyles,
-  };
-  autumnButton = () => {
-    let autumnTheme = "#C07F00";
-    this.setState({
-      ...articleStyles,
-      background: autumnTheme,
-    });
-  };
-  winterButton = () => {
-    let winterTheme = "#D4FAFC";
-    this.setState({
-      background: winterTheme,
-    });
-  };
-  springButton = () => {
-    let springTheme = "#A4D0A4";
-    this.setState({
-      background: springTheme,
-    });
-  };
-  summerButton = () => {
-    let summerTheme = "#FFD95A";
-    this.setState({
-      background: summerTheme,
-    });
-  };
+import { seasons } from './assets/data'
 
-  render() {
-    return (
-      <main>
-        <div style={this.state}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-            officiis amet aliquid aut nobis officia nemo odio dolorem incidunt
-            ex veniam, pariatur at, nesciunt perspiciatis explicabo nam, quis
-            beatae harum! Ut ad, beatae doloremque similique fugit reprehenderit
-            debitis atque, quia aut illum neque laboriosam, libero nemo. Nam,
-            officia tenetur, reiciendis eaque aspernatur dignissimos neque quae
-            illo expedita velit perspiciatis iusto, quibusdam maiores quidem
-            earum? Eum, ad. Doloremque expedita sequi quam, ipsum consequatur id
-            assumenda odit dolorem. Consequatur, provident. Possimus eligendi
-            laboriosam blanditiis doloribus sit, accusamus omnis odio maiores
-            eveniet temporibus iste nostrum, magnam vel adipisci necessitatibus
-            laudantium, quis voluptatum aliquam!
-          </p>
-        </div>
-        <button style={{...buttonStyles, background: buttonThemes.autumn}} onClick={this.autumnButton}>autumn</button>
-        <button style={{...buttonStyles, background: buttonThemes.winter}} onClick={this.winterButton}>winter</button>
-        <button style={{...buttonStyles, background: buttonThemes.spring}} onClick={this.springButton}>spring</button>
-        <button style={{...buttonStyles, background: buttonThemes.summer}} onClick={this.summerButton}>summer</button>
-      </main>
-    );
+export default function App(){
+  const [index, setIndex] = useState(0);
+
+  let hasNext = index < seasons.length - 1;
+  let hasPrevious = index > 0;
+
+  function handleNextClick(){
+    setIndex(index + 1)
   }
-}
+  function handlePreviousClick(){
+    setIndex(index - 1);
+  }
+  
+  let renderedSeason = seasons[index];
 
-export default App;
+  return (
+    <main style={{background : renderedSeason.background}}>
+      <h1>{renderedSeason.name}</h1>
+      <p>{renderedSeason.text}</p>
+      <button onClick={handlePreviousClick} disabled={!hasPrevious}> change to previous season</button>
+      <button onClick={handleNextClick} disabled={!hasNext}> change to next season</button>
+    </main>
+  )
+}
---
+++ day-9-conditional-rendering/season-picker-background/src/assets/data.jsx
@@ -0,0 +1,22 @@
+export const seasons = [
+  {
+    name: 'autumn',
+    text: 'now the season is autumn',
+    background: '#E57C23'
+  },
+  {
+    name: 'winter',
+    text: 'now the season is winter',
+    background: '#AEE2FF'
+  },
+  {
+    name: 'spring',
+    text: 'now the season is spring',
+    background: '#FFE569'
+  },
+  {
+    name: 'summer',
+    text: 'now the season is summer',
+    background: '#FFD95A'
+  }
+]
